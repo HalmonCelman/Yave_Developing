68a4e7dda1b397feaf5340313d408258ea7ee300
HalmonCelman <halmoncelman@gmail.com>
2023-11-16 10:29:07 +0100

    Celebrating 100th commit in Yave!
    - dancing and rotating meeples aroud you

--- main.cpp
+++ main.cpp
@@ -3,69 +3,78 @@ YAVE - by MAKOUS and KK
 */
 
 #include <stdint.h>
-#include <ctime>
+#include <cmath>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 #include "API/yave.h"
 
+// configuration of example
+const int numberOfMeeples=10;
+const int XTerrain = 12;
+const int ZTerrain = 12;
+const float distanceOfMeeples=10;
+
+// some configuration for rotate meeples function
+int totalCount=0;
+auto lastt=std::chrono::high_resolution_clock::now();
+const int dt=10;
+const double Period=10000;
+
+Object meeple[numberOfMeeples];
+Object terrain[XTerrain][ZTerrain];
+
+void RenderLoop(void);      //main loop
+void RotateMeeples(void);   //thread 1
+
 int main()
 {
+    // initializations
     Yave_Init();    //init engine
 
     Sound_CreateListener(); //init music
+    cam.Position={0,1.0f,0};
     UpdateListener();
-    
-    Object obj1,obj2;
-    obj1.isAnimated=true;
-    obj1.ModelFile="animations/Ymca_Dance/Ymca_Dance.dae";
-    obj1.AnimationFile="animations/Ymca_Dance/Ymca_Dance.dae";
-    obj1.SoundFile="music/YMCA.wav";
-    obj1.position={-2.0f,0.0f,-11.0f};
-    obj1.Init();
 
-
-
-    obj2.isAnimated=true;
-    obj2.ModelFile="animations/Flair/Flair.dae";
-    obj2.AnimationFile="animations/Flair/Flair.dae";
-    obj2.noSound = true;
-    obj2.Init();
-
-    Object terrain[12][12];
+    float meepleAngle;
+    for(int i=0;i<numberOfMeeples;i++){
+        if(i)
+            meepleAngle=2*3.14/numberOfMeeples*i;
+        else
+            meepleAngle=0;
+        meeple[i].isAnimated=true;
+        meeple[i].noSound=true;
+        meeple[i].ModelFile="animations/Flair/Flair.dae";
+        meeple[i].AnimationFile="animations/Flair/Flair.dae";
+        meeple[i].position={(float)(distanceOfMeeples*sin(meepleAngle)),0,(float)(distanceOfMeeples*cos(meepleAngle))};
+        meeple[i].Init();
+    }
 
     srand(time(0));         //just to get random colors
 
-    for(uint8_t i=0;i<12;i++){
-        for(uint8_t j=0;j<12;j++){
+    for(uint8_t i=0;i<XTerrain;i++){
+        for(uint8_t j=0;j<ZTerrain;j++){
             terrain[i][j].color[0]=(rand()%1000)/1000.0f;
             terrain[i][j].color[1]=(rand()%1000)/1000.0f;
             terrain[i][j].color[2]=(rand()%1000)/1000.0f;
             terrain[i][j].noSound=true;
             terrain[i][j].ModelFile="models/cube.glb";
-            terrain[i][j].position={11-(float)2*i,-1.0f,-(float)2*j};
+            terrain[i][j].position={XTerrain-1-(float)2*i,-1.0f,ZTerrain-1-(float)2*j};
             terrain[i][j].Init();
         }
     }
 
-    obj1.PlaySound(); // ! remember to put PlaySound() just before render loop - not earlier
-    cam.Position[1]=1.0f;
-    while (YAVE_activeWindow())
-    {
-        YAVE_executeEvents();
-        YAVE_prepareRender();
-
-        UpdateListener();
-
-        obj1.Render();
-        obj2.Render();
-
-        for(uint8_t i=0;i<12;i++){
-            for(uint8_t j=0;j<12;j++){
-                terrain[i][j].Render();
-            }
-        }
-    }
-
     
+
+
+    // *****************  main program **************************
+
+    std::thread th1(RotateMeeples);
+    
+    RenderLoop();
+    
+    // *********************** end **********************
 
     Sound_DeleteListener();
     Yave_End();
@@ -73,4 +82,42 @@ int main()
 }
 
 
+void RenderLoop(void){
+    while (YAVE_activeWindow())
+    {
+        YAVE_executeEvents();
+        YAVE_prepareRender();
 
+        UpdateListener();
+        for(int i=0;i<numberOfMeeples;i++){
+            meeple[i].Render();
+        }
+
+        for(uint8_t i=0;i<XTerrain;i++){
+            for(uint8_t j=0;j<ZTerrain;j++){
+                terrain[i][j].Render();
+            }
+        }
+
+    }
+}
+
+void RotateMeeples(void){
+    while(1){
+        auto nowt=std::chrono::high_resolution_clock::now();
+        auto deltat = std::chrono::duration_cast<std::chrono::milliseconds>(nowt - lastt);
+        
+        if( deltat.count() >= dt){
+        for(int i=0;i<numberOfMeeples;i++){
+            double angle = 2*3.14/numberOfMeeples*i+2*3.14*totalCount*dt/Period;
+            meeple[i].position[0] = distanceOfMeeples * sin(angle);
+            meeple[i].position[2] = distanceOfMeeples * cos(angle);
+        }
+        totalCount++;
+        if(2*3.14*totalCount*dt/Period>=Period){
+            totalCount=0;
+        }
+        lastt= std::chrono::high_resolution_clock::now();
+        }
+    }
+}
